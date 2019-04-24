90996de9f05e4af54dd3a8087f47bddcf11bf97d
picinatto <ederli.picinatto@gmail.com>
2019-04-24 13:43:27 -0300

    Finalizado fluxo de caixa pagamentos

--- FCL_pagamentos_conta.sql
+++ FCL_pagamentos_conta.sql
@@ -1,25 +1,24 @@
 SELECT 
-		t1.DocNum, 
+		t1.DocNum,
+		t0.LineId,
 		t1.DocType, 
 		t1.DocDate,
 		NULL AS CardCode,
 		NULL AS CardName,
 		t1.DocTotal,
-		t1.TransId AS OJDT_Doc,
+		t1.TransId AS DocTransId,
 		CASE
 			WHEN t1.CashAcct IS NOT NULL THEN t1.CashAcct
-			WHEN t2.CheckAct IS NOT NULL THEN t2.CheckAct
+			WHEN t1.CheckSum > 0		 THEN '1.01.01.03.01'
 			WHEN t1.BankAcct IS NOT NULL THEN t1.BankAcct
-										 ELSE t1.TrsfrAcct
+			ELSE t1.TrsfrAcct
 			END AS PayAcct,
-		t0.LineId,
 		t0.AcctCode,
 		t0.SumApplied,
 		t0.OcrCode
 
 FROM	VPM4 t0
 		INNER JOIN OVPM t1 ON t1.DocNum	  = t0.DocNum		--JOIN CABECALHO
-		LEFT JOIN  VPM1 t2 ON t2.DocNum	  = t0.DocNum		--JOIN CHEQUE
 
 WHERE 
 			t1.Canceled = 'N' 
---
+++ FCL_pagamentos_fornecedores.sql
@@ -0,0 +1,36 @@
+SELECT 
+		t1.DocNum,
+		t0.InvoiceId AS LineId,
+		t1.DocType,
+		t1.DocDate,
+		t1.CardCode,
+		t1.CardName,
+		t1.DocTotal,
+		t0.DocTransId,
+		CASE
+			WHEN t1.CashAcct IS NOT NULL THEN t1.CashAcct
+			WHEN t1.CheckSum > 0		 THEN '1.01.01.03.01'
+			WHEN t1.BankAcct IS NOT NULL THEN t1.BankAcct
+			ELSE t1.TrsfrAcct
+			END AS PayAcct,
+		t3.Account AS AccountCode,
+		(
+			(t3.Debit - t3.Credit) --Saldo valor da linha da conta contábil
+			/  (SELECT SUM(Debit) - SUM(Credit) --Dividido pelo saldo de todo lançamento contábil
+					FROM JDT1 
+					WHERE TransId = t0.DocTransId 
+							AND Account <> '2.01.01.01.01'
+				)
+		) 
+				* t0.SumApplied  --Multiplicado pelo valor da baixa 
+				AS SumApplied,
+		t3.OcrCode
+
+FROM	VPM2 t0
+		INNER JOIN OVPM t1 ON t1.DocNum	  = t0.DocNum		--JOIN CABECALHO
+		LEFT JOIN  OJDT t2 ON t2.TransId  = t0.DocTransId	--JOIN CABECALHO LCTO CONTABIL
+		LEFT JOIN (SELECT TransId, Account, ProfitCode AS OcrCode, Debit, Credit 
+					FROM JDT1 WHERE Account <> '2.01.01.01.01') t3 ON t3.TransId = t2.TransId   --JOIN LINHA LCTO CONTABIL
+
+WHERE		t1.Canceled = 'N'
+		AND t1.DocType <> 'A'
